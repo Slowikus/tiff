21ea1370617d3aa62eeb6fc50125e0099e861d6a
ArkadiuszK99 <kacperski.arkadiusz1@gmail.com>
2021-04-18 19:04:23 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import tiff_manipulations as tf
+import tiff as tf2
+import matplotlib.pyplot as plt
+
+
+image = 'kilka_pikseli.tif'
+# image = 'dupa.txt'
+
+tiff_file = tf2.TiffManipulations(image)
+# tiff_file.read_data()
+# list_data()
+# print(tiff_file.check_required_fields())
+
+# img = plt.imread(image)
+# plt.imshow(img)
+# plt.show()
