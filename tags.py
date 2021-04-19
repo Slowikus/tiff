56d8a0bf4f13c4925b7055937e67e24e723078ef
ArkadiuszK99 <kacperski.arkadiusz1@gmail.com>
2021-04-19 21:15:07 +0200

    Create tags.py

---
+++ tags.py
@@ -0,0 +1,15 @@
+basicTags = {'width': 0x0100,
+        'length': 0x0101,
+        'bits_per_sample': 0x102,
+        'compression': 0x0103,
+        'photometric_interpretation': 0x0106,
+        'strip_offsets': 0x111,
+        'rows_per_strip': 0x0116,
+        'strip_byte_counts': 0x117,
+        'x_resolution': 0x011A,
+        'y_resolution': 0x11B,
+        'resolution_unit': 0x0128}
+
+moreTags = {
+
+        }
