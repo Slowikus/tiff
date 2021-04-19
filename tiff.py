a7d580f513e21193d2c9faf05052550674499507
ArkadiuszK99 <kacperski.arkadiusz1@gmail.com>
2021-04-19 21:13:54 +0200

    Update tiff.py

--- tiff.py
+++ tiff.py
@@ -1,45 +1,40 @@
-class TiffManipulations:
+import sys
+import tags
+
+class Tiff:
     def __init__(self, file_name):
-        self.hexCode = {'width': 0x0100, 'length': 0x0101, 'bits_per_sample': 0x102, 'compression': 0x0103,
-                        'photometric_interpretation': 0x0106, 'strip_offsets': 0x111, 'rows_per_strip': 0x0116,
-                        'strip_byte_counts': 0x117, 'x_resolution': 0x011A, 'y_resolution': 0x11B,
-                        'resolution_unit': 0x0128}
 
-        self.hexValue = {'width': 0, 'length': 0, 'bits_per_sample': 0, 'compression': 0,
-                         'photometric_interpretation': 0, 'strip_offsets': 0, 'rows_per_strip': 0,
-                         'strip_byte_counts': 0, 'x_resolution': 0, 'y_resolution': 0,
-                         'resolution_unit': 0}
-
-        self.data_hex_list = self.returnHexList(file_name)
+        self.hexList = self.returnHexList(file_name)
         self.endian = self.defEndian()
         self.isTiff = self.isTiff()
-        self.offset = self.connectByte(self.data_hex_list[4: 8])
+        self.offset = self.connectByte(self.hexList[4: 8])
+        print("offset: " + str(self.offset))
         self.returnIFD(self.offset)
 
 
     @staticmethod
     def returnHexList(file_name):
+        hexList = []
         file = open(file_name, "rb")
         data = file.read()
         file.close()
-        data_hex = data.hex()
-        data_hex_list = []
-        for i in range(len(data_hex)):
+        hex = data.hex()
+
+        for i in range(len(hex)):
             if i % 2 == 0 and i != 0:
-                data_hex_list.append(data_hex[i - 2:i])
-        # print("data_hex_list:" + str(data_hex_list))
-        return data_hex_list
+                hexList.append(hex[i - 2:i])
+        return hexList
 
     def defEndian(self):
-        if int(self.data_hex_list[0] + self.data_hex_list[1], 16) == 0x4949:
+        if int(self.hexList[0] + self.hexList[1], 16) == 0x4949:
             return "little"
-        elif int(self.data_hex_list[0] + self.data_hex_list[1]) == 0x4d4d:
+        elif int(self.hexList[0] + self.hexList[1], 16) == 0x4d4d:
             return "big"
         else:
             raise NameError("This file is not Tiff")
 
     def isTiff(self):
-        if self.connectByte(self.data_hex_list[2: 4]) == 0x002A:
+        if self.connectByte(self.hexList[2: 4]) == 0x002A:
             return True
         raise NameError("This file is not Tiff")
 
@@ -57,21 +52,15 @@ class TiffManipulations:
                 connectedByte = tab[3] + tab[2] + tab[1] + tab[0]
         return int(connectedByte, 16)
 
-    def returnIFD(self, start):
-        DE = self.connectByte(self.data_hex_list[start: start + 2])
-        for i in range(0, DE):
-            self.returnDE(start + 2 + i*12)
-        nextIFDoffset = start + 14 + DE*12
-        print("nextIFDoffset: " + str(nextIFDoffset))
-        if nextIFDoffset != 0:
-            self.returnIFD(nextIFDoffset)
-        print("DONE")
-
     def returnDE(self, start):
-        tag = self.connectByte(self.data_hex_list[start: start+2])
-        type = self.connectByte(self.data_hex_list[start+2: start+4])
-        size = self.connectByte(self.data_hex_list[start+4: start+8])
-        data = self.connectByte(self.data_hex_list[start+8: start+12])
+        tag = self.connectByte(self.hexList[start: start + 2])
+        type = self.connectByte(self.hexList[start + 2: start + 4])
+        size = self.connectByte(self.hexList[start + 4: start + 8])
+        data = self.connectByte(self.hexList[start + 8: start + 12])
+
+        for i in tags.basicTags:
+            if tags.basicTags[i] == tag:
+                print("tag name: " + str(i))
 
         print("tag: " + str(tag))
         print("type: " + str(type))
@@ -79,3 +68,18 @@ class TiffManipulations:
         print("data: " + str(data))
 
         print("----------")
+
+    def returnIFD(self, start):
+        amountOfDE = self.connectByte(self.hexList[start: start + 2])
+        print("Amount of TAGS:" + str(amountOfDE))
+        for i in range(0, amountOfDE):
+            self.returnDE(start + 2 + i*12)
+        sizeOfIFD = start + 2 + amountOfDE * 12
+        offsetNextIFD = self.connectByte(self.hexList[sizeOfIFD:  sizeOfIFD + 4])
+        print("offset of next IFD: " + str(offsetNextIFD))
+        if offsetNextIFD == 0:
+            print("End of file")
+            sys.exit()
+        else:
+            print("Go to next IFD")
+            self.returnIFD(sizeOfIFD+4)
